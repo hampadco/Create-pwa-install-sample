5b032b2689b8c9100fab789f7c6ad9aff332cf14
hampadco <tak1.ghasemi@gmail.cm>
2023-03-07 18:13:45 +0330

    16esfand

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace Refactor.Controllers;
 public class HomeController : Controller
 {
    private readonly IHubContext<DataHub> _hubContext;
+   
 
         public HomeController(IHubContext<DataHub> hubContext)
         {
@@ -17,6 +18,8 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+          
+            
         return View();
     }
     
@@ -43,11 +46,72 @@ public class HomeController : Controller
             ///dev/ttyUSB0
             using (SerialPort serialPort = new SerialPort("COM10"))
             {
+                    serialPort.BaudRate = 19200;
+                    serialPort.Parity = Parity.Odd;
+                    serialPort.DataBits = 8;
+                    serialPort.StopBits = StopBits.One;
+
                 serialPort.Open();
+
+                  byte[] tx_Data = new byte[10];
+                    tx_Data[0]= 0xFA;
+                    tx_Data[1] = 0x0A;
+                    tx_Data[2] = 0x03;
+                    tx_Data[3] = 0x01;
+                    tx_Data[4] = 0x01;
+                    tx_Data[5] = 0x00;
+                    tx_Data[6] = 0x00;
+                    tx_Data[7] = 0x00;
+                    tx_Data[8] = 0x00;
+                    tx_Data[9] = 0x0F;
+                    serialPort.Write(tx_Data, 0, tx_Data.Length);
+
+                    byte[] buffer = new byte[17];
+                    string data="0";
+                    string data1="0";
+                    string data2="0";
+
+
                 while(true)
                 {
-                    string data = serialPort.ReadLine();
-                    await _hubContext.Clients.All.SendAsync("ReceiveData", data);
+                     while (serialPort.BytesToRead > 14)
+                 {
+                      buffer[1] = (byte)serialPort.ReadByte();
+
+                        if (buffer[1] == 0xFA) //250
+                         {
+
+                                buffer[2] = (byte)serialPort.ReadByte();    
+                                buffer[3] = (byte)serialPort.ReadByte();
+                                serialPort.Read(buffer, 4, 10);
+
+                                if (buffer[2] == 13 && buffer[4] == 4 && buffer[5] == 132)
+                                {
+                                    Task.Run(() =>
+                                    {
+                                        if (buffer[10] == 255) data="30";
+                                        else data=(buffer[10] * 3).ToString();
+                                    }).Wait();
+                                }
+
+                               if (buffer[2] == 17 && buffer[4] == 4 && buffer[5] == 133)
+                                {
+                                    int pulse = (buffer[11] << 8) | buffer[10];
+                                    int spo = buffer[12];
+                                    if (pulse == 511) Task.Run(() =>data1=null).Wait();
+                                    else Task.Run(() =>data1=(pulse).ToString()).Wait();
+                                    if (spo == 127) Task.Run(() =>data2=(null)).Wait();
+                                    else Task.Run(() => data2=(spo).ToString()).Wait();
+                                }
+
+                         }     
+
+                      
+                    await _hubContext.Clients.All.SendAsync("ReceiveData", data,data1,data2);
+
+
+                   
+                }
                 }
 
 
--- Models/DataHub.cs
+++ Models/DataHub.cs
@@ -2,12 +2,12 @@ using Microsoft.AspNetCore.SignalR;
 
 public class DataHub : Hub
     {
-        public async Task SendData(string data)
+        public async Task SendData(string data,string data1,string data2)
         {
             //write data to serial port
             System.Console.WriteLine("Listening to serial port");
 
     
-            await Clients.All.SendAsync("ReceiveData", data);
+            await Clients.All.SendAsync("ReceiveData", data,data1,data2);
         }
     }
